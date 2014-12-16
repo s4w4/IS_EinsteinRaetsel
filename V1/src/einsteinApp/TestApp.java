b2b8f01acb209541661535030de1580e955460c6
sawa112 <AlexanderSawadski@gmail.com>
2014-12-17 00:37:55 +0100

    TestApp + Kommentare

--- V1/src/einsteinApp/TestApp.java
+++ V1/src/einsteinApp/TestApp.java
@@ -12,36 +12,59 @@ import java.util.List;
 
 public class TestApp {
     public static void main(String[] args){
+        /**
+         * Definitionsbereich erstellen
+         */
         Definition dX = new Definition();
         dX.addValue(1);
-        dX.addValue(2);
-        dX.addValue(3);
-        dX.addValue(4);
-        dX.addValue(5);
         Definition dY = new Definition();
         dY.addValue(1);
+        dY.addValue(2);
+        dY.addValue(3);
+        dY.addValue(4);
+        dY.addValue(5);
 
+        /**
+         * Variablen / Knoten erstellen
+         */
         Vertex v1 = new Vertex("x", dX);
         Vertex v2 = new Vertex("y", dY);
-        v1.setRang(1);
-        v1.setRang(2);
 
         List<Vertex> vertexList = new ArrayList<>();
         vertexList.add(v1);
         vertexList.add(v2);
 
+        /**
+         * Binäre Constraints erstellen
+         */
         BinaryConstraint cB = new ConstraintNotEqual();
         List<BinaryConstraint> cList = new ArrayList<>();
         cList.add(cB);
 
+        /**
+         * Kanten erstellen
+         */
         Arc arc1 = new Arc(v1, v2, cList);
         Arc arc2 = new Arc(v2, v1, cList);
         List<Arc> arcList = new ArrayList<>();
         arcList.add(arc1);
         arcList.add(arc2);
 
+        /**
+         * Sortieren
+         */
+        v1.setRang(1);
+        v2.setRang(2);
+
+        /**
+         * Starten
+         */
         System.out.println(arcList);
-        ConstraintSolver.ac3la(v2, arcList);
+        ConstraintSolver.ac3la(v1, arcList);
+
+        /**
+         * Ausgabe
+         */
         System.out.println();
         System.out.println(arcList);
 
